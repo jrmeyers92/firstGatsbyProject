e319696eef613951460d68d9c0452263d358f791
Jake Meyers <jakejake92@yahoo.com>
2021-01-19 18:29:05 -0700

    edited products page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,6 +6,14 @@ const blog = () => {
   return (
     <Layout>
       <h1>This is our blog page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae
+        libero fugiat quibusdam amet deleniti tempore, tenetur culpa architecto
+        possimus. Optio, commodi reiciendis laborum ratione accusantium hic
+        nulla dolorum iusto repudiandae delectus cupiditate aut non ab. Vero,
+        corporis unde, ratione architecto autem at libero nobis maiores a enim
+        ad praesentium iste?
+      </p>
     </Layout>
   )
 }
--- src/pages/products.js
+++ src/pages/products.js
@@ -5,6 +5,20 @@ const products = () => {
   return (
     <Layout>
       <h1>this is our products page</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetkur adipisicing elit. Est eaque
+        ullam tempora facilis, suscipit repellendus aut distinctio explicabo
+        fugit quos accusantium placeat fugiat molestias doloremque! Nam
+        consequuntur tempora pariatur, ducimus incidunt sunt cupiditate neque
+        similique non corporis mollitia nisi laborum laboriosam animi delectus
+        at possimus nihil quas provident perspiciatis? Eos inventore at
+        doloremque distinctio modi eum dignissimos, est quam! Nihil, eum.
+        Nesciunt dolor, quibusdam at et excepturi quae consequuntur sunt?
+        Obcaecati adipisci eveniet iste deleniti at voluptates maiores quis amet
+        vitae eaque commodi fugit, ea assumenda delectus voluptatibus! Placeat
+        deleniti temporibus laborum ipsum totam maxime est mollitia
+        reprehenderit aliquid at!
+      </p>
     </Layout>
   )
 }
